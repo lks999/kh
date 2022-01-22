dea484eefdce8dd9c558509bb3da1dacadf4cd22
lks999 <65687385+lks999@users.noreply.github.com>
2022-01-22 22:49:47 +0900

    Add files via upload

---
+++ product/model/vo/Product.java
@@ -0,0 +1,9 @@
+package product.model.vo;
+
+public class Product {
+	
+	public void printProduct() {
+		System.out.println("이 TV는 42인치이고 100만원에 판매하고 있습니다!");
+	}
+
+}
---
+++ product/run/TestProduct.java
@@ -0,0 +1,13 @@
+package product.run;
+
+import product.model.vo.Product;
+
+public class TestProduct {
+	
+	public static void main(String[] args) {
+		
+		Product main = new Product();
+		main.printProduct();
+	}
+
+}
