01ae45758ac593d92914fc54ba874cfa4f979eaf
lks999 <65687385+lks999@users.noreply.github.com>
2022-01-25 01:03:39 +0900

    Add files via upload

---
+++ function/run/Main.java
@@ -0,0 +1,15 @@
+package com.kh.function.run;
+
+import com.kh.function.Example;
+
+public class Main {
+	public static void main(String[] args) {
+		Example main = new Example();
+		
+//		main.opSample1();
+//		main.opSample2();
+//		main.opSample3();
+		main.opSample4();
+	}
+
+}
